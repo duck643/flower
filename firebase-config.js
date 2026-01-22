fbd540cdb580e30c59fa64aecd18b763677a5006
duck643 <mancurovae6@gmail.com>
2026-01-22 18:37:52 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyADk_VRBvOU5ZYU1uwKnnc1mVJ_Y1vRvJs",
+  authDomain: "flower-d8b31.firebaseapp.com",
+  projectId: "flower-d8b31",
+  storageBucket: "flower-d8b31.firebasestorage.app",
+  messagingSenderId: "68069184847",
+  appId: "1:68069184847:web:f62559172990d6ba25e13a"
+};
+
+export { firebaseConfig };
